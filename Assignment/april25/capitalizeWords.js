9553f012fb161aa4071a87d10f84bf7a0c531e6e
Saranya B <saranyauma17@gmail.com>
2021-06-03 01:49:12 +0530

    Completed

--- Assignment/april25/capitalizeWords.js
+++ Assignment/april25/capitalizeWords.js
@@ -1,3 +1,9 @@
+/*
+Q6. Write a JavaScript function to capitalize the first letter of each word in a string?
+File name suggestion: capitalizeWords.js
+Test Data :
+console.log(capitalizeWords(‘js string exercises’)); // “Js String Exercises”
+*/
 const capitalizeWords = (str) => {
     for(var ind = 0;ind<str.length;ind++) {
         if(ind==0 || (str.charAt(ind-1)==' ' &&(str.charAt(ind)>='a'&& str.charAt(ind)<='z'))) {
